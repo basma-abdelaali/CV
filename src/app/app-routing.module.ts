ab8aed508dbbc0b540bc2ca356b1b2b533ad0a17
hatemAttia <attiahatem@outlook.com>
2021-08-05 13:44:03 +0100

    first commit cv

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+    {
+      path: '',
+      component: HomeComponent,
+    }]
+  
+  
+  @NgModule({
+    declarations: [],
+    imports: [CommonModule, RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+
+export class AppRoutingModule { }
